f7135d903e27b491091eef591959cef6aec2d2ad
Brendon Smith <br3ndonland@protonmail.com>
2020-04-18 16:55:15 -0400

    Add styles from Vue Mastery
    https://github.com/Code-Pop/Unit-Testing

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -24,4 +24,13 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.app-header {
+  min-height: 40px;
+  padding: 0.2em 1em;
+  background: white;
+}
+button {
+  float: right;
+}
+</style>

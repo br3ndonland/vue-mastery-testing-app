7d8cb76c6846bfc4eb128a06084911e6ee9b2fd9
Brendon Smith <br3ndonland@protonmail.com>
2020-04-18 16:55:35 -0400

    Import AppHeader in App.vue
    Although the title of _AppHeader.vue_ is `PascalCase`, we can apparently
    use the `kebab-case` variant directly in the template, `<app-header />`,
    without having to explicitly tell Vue.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <app-header />
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -7,6 +8,15 @@
     <router-view />
   </div>
 </template>
+
+<script>
+import AppHeader from "@/components/AppHeader"
+export default {
+  components: {
+    AppHeader,
+  },
+}
+</script>
 
 <style>
 #app {

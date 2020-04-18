7c6ed820f5c03d7a8f8b150bf8bcf4586baebd98
Brendon Smith <br3ndonland@protonmail.com>
2020-04-18 15:16:34 -0400

    Add AppHeader component
    Created with my Vue snippet:
    https://github.com/br3ndonland/dotfiles/blob/master/codium/User/snippets

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button v-show="loggedIn">Logout</button>
+  </div>
+</template>
+
+<script>
+// import OtherComponent from "OtherComponent.vue"
+// import { Mixin } from "mixins/Mixin.js"
+export default {
+  name: "AppHeader",
+  components: {},
+  mixins: [],
+  props: {},
+  data() {
+    return {
+      loggedIn: false,
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  methods: {},
+}
+</script>
+
+<style scoped></style>

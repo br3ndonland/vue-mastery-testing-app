4c95393c82159ac7bf421516a3ad54fc25d5afb9
Brendon Smith <br3ndonland@protonmail.com>
2020-04-27 13:21:38 -0400

    Create test suite and set up tests for lesson 3

---
+++ tests/unit/RandomNumber.spec.js
@@ -0,0 +1,8 @@
+import { mount } from "@vue/test-utils"
+import RandomNumber from "@/components/RandomNumber"
+
+describe("RandomNumber", () => {
+  test("Check default value: randomNumber default should be 0", () => {})
+  test("Simulate interaction: button -> 0 < randomNumber < 10", () => {})
+  test("Change prop values: new min < randomNumber < new max", () => {})
+})

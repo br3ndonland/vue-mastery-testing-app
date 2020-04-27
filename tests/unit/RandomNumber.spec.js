8bd1c55288d344ead593a0207da560d57a0418d2
Brendon Smith <br3ndonland@protonmail.com>
2020-04-27 19:41:30 -0400

    Complete tests for lesson 3
    Three tests for random number component

--- tests/unit/RandomNumber.spec.js
+++ tests/unit/RandomNumber.spec.js
@@ -2,7 +2,29 @@ import { mount } from "@vue/test-utils"
 import RandomNumber from "@/components/RandomNumber"
 
 describe("RandomNumber", () => {
-  test("Check default value: randomNumber default should be 0", () => {})
-  test("Simulate interaction: button -> 0 < randomNumber < 10", () => {})
-  test("Change prop values: new min < randomNumber < new max", () => {})
+  test("Check default value: randomNumber default should be 0", () => {
+    const wrapper = mount(RandomNumber)
+    expect(wrapper.html()).toContain("<span>0</span>")
+  })
+  test("Simulate interaction: button -> 0 < randomNumber < 10", async () => {
+    const wrapper = mount(RandomNumber)
+    wrapper.find("button").trigger("click")
+    await wrapper.vm.$nextTick()
+    const randomNumber = parseInt(wrapper.find("span").element.textContent)
+    expect(randomNumber).toBeGreaterThanOrEqual(1)
+    expect(randomNumber).toBeLessThanOrEqual(10)
+  })
+  test("Change prop values: new min < randomNumber < new max", async () => {
+    const wrapper = mount(RandomNumber, {
+      propsData: {
+        min: 200,
+        max: 300,
+      },
+    })
+    wrapper.find("button").trigger("click")
+    await wrapper.vm.$nextTick()
+    const randomNumber = parseInt(wrapper.find("span").element.textContent)
+    expect(randomNumber).toBeGreaterThanOrEqual(200)
+    expect(randomNumber).toBeLessThanOrEqual(300)
+  })
 })

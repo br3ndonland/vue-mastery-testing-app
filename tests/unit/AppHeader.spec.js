4a27803a3395e96088c1748f56d33204b025f8c7
Brendon Smith <br3ndonland@protonmail.com>
2020-05-02 18:34:15 -0400

    Refactor Jest syntax from test() to it()
    I like it(). It's like a checklist for your component.

--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -2,13 +2,13 @@ import { mount } from "@vue/test-utils"
 import AppHeader from "@/components/AppHeader"
 
 describe("AppHeader", () => {
-  test("If user is logged in, show logout button", async () => {
+  it("shows the logout button if user is logged in", async () => {
     const wrapper = mount(AppHeader)
     wrapper.setData({ loggedIn: true })
     await wrapper.vm.$nextTick()
     expect(wrapper.find("button").isVisible()).toBe(true)
   })
-  test("If user is not logged in, do not show logout button", async () => {
+  it("doesn't show logout button if user is not logged in", async () => {
     const wrapper = mount(AppHeader)
     // loggedIn is false by default
     // We're using the default, so we don't have to wait for component to mount
--- tests/unit/RandomNumber.spec.js
+++ tests/unit/RandomNumber.spec.js
@@ -2,11 +2,11 @@ import { mount } from "@vue/test-utils"
 import RandomNumber from "@/components/RandomNumber"
 
 describe("RandomNumber", () => {
-  test("Check default value: randomNumber default should be 0", () => {
+  it("has the proper default value", () => {
     const wrapper = mount(RandomNumber)
     expect(wrapper.html()).toContain("<span>0</span>")
   })
-  test("Simulate interaction: button -> 0 < randomNumber < 10", async () => {
+  it("generates a number between min and max after button click", async () => {
     const wrapper = mount(RandomNumber)
     wrapper.find("button").trigger("click")
     await wrapper.vm.$nextTick()
@@ -14,7 +14,7 @@ describe("RandomNumber", () => {
     expect(randomNumber).toBeGreaterThanOrEqual(1)
     expect(randomNumber).toBeLessThanOrEqual(10)
   })
-  test("Change prop values: new min < randomNumber < new max", async () => {
+  it("has the correct prop values after props are changed", async () => {
     const wrapper = mount(RandomNumber, {
       propsData: {
         min: 200,

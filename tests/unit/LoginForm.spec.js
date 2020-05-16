6d5838ae47b4bab5be9b0dd8c2ebb00cf70f4efe
Brendon Smith <br3ndonland@protonmail.com>
2020-05-16 17:22:58 -0400

    Scaffold test for LoginForm in lesson 4

---
+++ tests/unit/LoginForm.spec.js
@@ -0,0 +1,13 @@
+import { mount } from "@vue/test-utils"
+import LoginForm from "@/components/LoginForm.vue"
+
+describe("LoginForm", () => {
+  it("emits an event with a user data payload", () => {
+    const wrapper = mount(LoginForm)
+    // 1. Find text input
+    // 2. Set value for text input
+    // 3. Simulate form submission
+    // 4. Assert event has been emitted
+    // 5. Assert payload is correct
+  })
+})

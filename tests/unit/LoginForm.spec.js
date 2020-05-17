cfc5d01536535515c4f1ed584bccf8296ce43184
Brendon Smith <br3ndonland@protonmail.com>
2020-05-17 18:27:46 -0400

    Find input element to test in lesson 4

--- tests/unit/LoginForm.spec.js
+++ tests/unit/LoginForm.spec.js
@@ -5,6 +5,7 @@ describe("LoginForm", () => {
   it("emits an event with a user data payload", () => {
     const wrapper = mount(LoginForm)
     // 1. Find text input
+    const input = wrapper.find('input[type="text"]')
     // 2. Set value for text input
     // 3. Simulate form submission
     // 4. Assert event has been emitted

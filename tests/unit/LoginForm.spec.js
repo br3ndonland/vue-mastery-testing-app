7599bac387fb5a4b8d938c994c1b9473f495a739
Brendon Smith <br3ndonland@protonmail.com>
2020-05-17 18:37:16 -0400

    Set value for login form text input in lesson 4

--- tests/unit/LoginForm.spec.js
+++ tests/unit/LoginForm.spec.js
@@ -7,6 +7,7 @@ describe("LoginForm", () => {
     // 1. Find text input
     const input = wrapper.find(`[data-testid="input-name"]`)
     // 2. Set value for text input
+    input.setValue("Dennis Nedry")
     // 3. Simulate form submission
     // 4. Assert event has been emitted
     // 5. Assert payload is correct

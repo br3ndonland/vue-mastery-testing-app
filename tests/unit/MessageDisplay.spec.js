49906b2c01fa22530a5c96db5e202ebcd4467272
Brendon Smith <br3ndonland@protonmail.com>
2020-05-18 18:34:21 -0400

    Mock API calls for MessageDisplay in lesson 5

--- tests/unit/MessageDisplay.spec.js
+++ tests/unit/MessageDisplay.spec.js
@@ -1,15 +1,22 @@
 import { mount } from "@vue/test-utils"
+import { getMessage } from "@/services/axios"
 import MessageDisplay from "@/components/MessageDisplay.vue"
 
 describe("MessageDisplay", () => {
   it("calls getMessage and displays message", async () => {
     // 1. Mock API call
+    const mockMessage = "This is a mock message from the database"
+    getMessage.mockResolvedValueOnce({ text: mockMessage })
+    const wrapper = mount(MessageDisplay)
     // 2. Await promise resolution
     // 3. Assert that API call happened once: `expect()`
     // 4. Assert that component displays message: `expect()`
   })
   it("displays an error if getMessage fails", async () => {
     // 1. Mock API call
+    const mockError = "getMessage failed to return the expected response."
+    getMessage.mockRejectedValueOnce(mockError)
+    const wrapper = mount(MessageDisplay)
     // 2. Await promise resolution
     // 3. Assert that API call happened once: `expect()`
     // 4. Assert that component displays message: `expect()`

eae6c9703c7d79bb2b90bb99a3b2382e6466ce2a
Brendon Smith <br3ndonland@protonmail.com>
2020-05-18 18:15:21 -0400

    Scaffold tests for MessageDisplay in lesson 5

---
+++ tests/unit/MessageDisplay.spec.js
@@ -0,0 +1,17 @@
+import { mount } from "@vue/test-utils"
+import MessageDisplay from "@/components/MessageDisplay.vue"
+
+describe("MessageDisplay", () => {
+  it("calls getMessage and displays message", async () => {
+    // 1. Mock API call
+    // 2. Await promise resolution
+    // 3. Assert that API call happened once: `expect()`
+    // 4. Assert that component displays message: `expect()`
+  })
+  it("displays an error if getMessage fails", async () => {
+    // 1. Mock API call
+    // 2. Await promise resolution
+    // 3. Assert that API call happened once: `expect()`
+    // 4. Assert that component displays message: `expect()`
+  })
+})

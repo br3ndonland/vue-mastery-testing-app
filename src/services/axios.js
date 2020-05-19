910114d609583c57d281d0d364461c820bc283fa
Brendon Smith <br3ndonland@protonmail.com>
2020-05-18 20:44:03 -0400

    Add try/catch to Axios API call in axios.js

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios"
 
 export async function getMessage() {
-  const r = await axios(`http://localhost:3000/message`)
-  return r.data
+  try {
+    const r = await axios(`http://localhost:3000/message`)
+    return r.data
+  } catch (e) {
+    throw Error(e)
+  }
 }

67b46ee810ad58c3047fd2c2ec885fc9941c0813
Brendon Smith <br3ndonland@protonmail.com>
2020-05-23 16:33:09 -0400

    Correct isVisible deprecation warning
    Warning:
    
    [vue-test-utils]: isVisible is deprecated and will be removed in the
    next major version. Consider a custom matcher such as those provided
    in jest-dom: https://github.com/testing-library/jest-dom#tobevisible.
    When using with findComponent, access the DOM element with
    findComponent(Comp).element.
    
    Solution:
    
    - Import jest-dom: https://github.com/testing-library/jest-dom
    - Extend expect() with toBeVisible() from jest-dom:
      https://jestjs.io/docs/en/expect#expectextendmatchers

--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -1,15 +1,18 @@
 import { mount } from "@vue/test-utils"
+import { toBeVisible } from "@testing-library/jest-dom/matchers"
 import AppHeader from "@/components/AppHeader"
+
+expect.extend({ toBeVisible })
 
 describe("AppHeader", () => {
   it("shows the logout button if user is logged in", async () => {
     const wrapper = mount(AppHeader)
     await wrapper.setData({ loggedIn: true })
-    expect(wrapper.find("button").isVisible()).toBe(true)
+    expect(wrapper.find("button").element).toBeVisible()
   })
   it("doesn't show logout button if user is not logged in", async () => {
     const wrapper = mount(AppHeader)
-    // loggedIn is false, so we don't have to wait for component to mount
-    expect(wrapper.find("button").isVisible()).toBe(false)
+    // loggedIn is false, so we don't have to use setData()
+    expect(wrapper.find("button").element).not.toBeVisible()
   })
 })
